630ebc9a365abea26a2a41419dbfc274dee0104a
athira.ram <athirasumara@gmail.com>
2023-02-27 13:24:08 +0530

    Second changes

--- app/src/main/java/com/example/demo1/MainActivity.java
+++ app/src/main/java/com/example/demo1/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
     }
 
     @Override
